0220066d28d9c0653c77e32a3b37f62149ec821c
LiuPeng <liupengs@foxmail.com>
2015-10-11 10:35:08 +0800

    Update Merge Sorted Array.c

--- Merge Sorted Array.c
+++ Merge Sorted Array.c
@@ -1,3 +1,4 @@
+//solution 1
 void merge(int* nums1, int m, int* nums2, int n) {
     int i,j,k;
     i=m-1;
@@ -17,5 +18,23 @@ void merge(int* nums1, int m, int* nums2, int n) {
         nums1[k]=nums2[j];
         k--;
         j--;
+    }
+}
+
+//solution 2
+void merge(int* nums1, int m, int* nums2, int n) {
+    int i,j,k;
+    i=m-1;
+    j=n-1;
+    k=m+n-1;
+    while(j>=0){
+        if(i>=0 && nums1[i]>=nums2[j]){
+            nums1[k]=nums1[i];
+            i--;
+        }else{
+            nums1[k]=nums2[j];
+            j--;
+        }
+        k--;
     }
 }
