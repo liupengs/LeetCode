4fc4c1a1200e856621e9037098cecb31ac12c592
LiuPeng <liupengs@foxmail.com>
2015-10-12 22:16:51 +0800

    Create Subsets.c

---
+++ Subsets.c
@@ -0,0 +1,15 @@
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        lists=[[]]
+        for i in nums:
+            list1=lists[:]
+            for t in list1:
+                t=sorted(t+[i])
+                if t not in lists:
+                    lists.append(t)
+        return lists
+                
