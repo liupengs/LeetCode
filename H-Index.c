2d4f956a3a7557501b387d2f4aa16e4b8db9031e
LiuPeng <liupengs@foxmail.com>
2015-10-21 18:38:52 +0800

    Update H-Index.c

--- H-Index.c
+++ H-Index.c
@@ -1,3 +1,4 @@
+//solution one
 int comp(const void*a,const void*b)
 {
     return *(int*)a-*(int*)b;
@@ -7,6 +8,25 @@ int hIndex(int* citations, int citationsSize) {
     qsort(citations,citationsSize,sizeof(int),comp);
     for(i=citationsSize-1,h=0;i>=0;i--){
         if(citations[i]>h) h++;
+    }
+    return h;
+}
+
+//solution two
+int hIndex(int* citations, int citationsSize) {
+    int c[citationsSize+1],i,h,a;
+    memset(c,0,sizeof(int)*(citationsSize+1));
+    for(i=0;i<citationsSize;i++){
+        if(citations[i]>=citationsSize) c[citationsSize]++;
+        else c[citations[i]]++;
+    }
+    for(i=citationsSize,h=0,a=0;i>0;i--){
+        a+=c[i];
+        if(a>=i && i>h){
+            h=i;
+        }else if(i>a && a>=h){
+            h=a;
+        }
     }
     return h;
 }
