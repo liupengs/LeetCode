f80709d39cd6f1d2d7a8a5bf1a1e36cea5ce90b9
LiuPeng <liupengs@foxmail.com>
2015-10-25 22:32:31 +0800

    Update Restore IP Addresses.c

--- Restore IP Addresses.c
+++ Restore IP Addresses.c
@@ -1,3 +1,4 @@
+#one
 class Solution(object):
     def testLeft(self,s,b,lens,finds,r):
         #print finds
@@ -33,4 +34,31 @@ class Solution(object):
                 self.testLeft(s,i,lens,t,2)
             else:
                 break
+        return self.re
+        
+#two
+class Solution(object):
+    def testLeft(self,s,b,lens,finds,r):
+        if r==5 and b==lens:
+            self.re.append(".".join(finds))
+            return
+        for i in xrange(b+1,lens+1):
+            t=s[b:i]
+            if len(t)>1 and t[0]=="0":
+                break
+            t1=int(t)
+            if t1<256:
+                self.testLeft(s,i,lens,finds+[t],r+1)
+            else:
+                break
+    def restoreIpAddresses(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        lens=len(s)
+        if lens>12:
+            return []
+        self.re=[]
+        self.testLeft(s,0,lens,[],1)
         return self.re
