c735045dd62e717cc819dbda7d06abaadca4be6f
LiuPeng <liupengs@foxmail.com>
2016-02-22 22:14:44 +0800

    Update Clone Graph.py

--- Clone Graph.py
+++ Clone Graph.py
@@ -1,28 +1,16 @@
-# Definition for a undirected graph node
-# class UndirectedGraphNode(object):
-#     def __init__(self, x):
-#         self.label = x
-#         self.neighbors = []
-
 class Solution(object):
     def clone(self, node, temp):
-        result =None
-        if node == None:
-            return None
-        else:
-            result = UndirectedGraphNode(node.label)
-            temp[node.label]=result
-            for i in node.neighbors:
-                if temp.has_key(i.label):
-                    result.neighbors.append(temp[i.label])
-                else:
-                    result.neighbors.append(self.clone(i,temp))
+        result = UndirectedGraphNode(node.label)
+        temp[node.label]=result
+        for i in node.neighbors:
+            if temp.has_key(i.label):
+                result.neighbors.append(temp[i.label])
+            else:
+                result.neighbors.append(self.clone(i,temp))
         return result
     def cloneGraph(self, node):
-        """
-        :type node: UndirectedGraphNode
-        :rtype: UndirectedGraphNode
-        """
         temp={}
-        
-        return self.clone(node,temp)
+        if node:
+            return self.clone(node,temp)
+        else:
+            return None
